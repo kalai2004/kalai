d449b0006f4a159e208ffdd5b76c5a2299b512fb
msgpc <106960497+msgpc@users.noreply.github.com>
2022-07-16 15:43:28 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -12,6 +12,7 @@ from bot.helper.mirror_utils.status_utils.clone_status import CloneStatus
 from bot.helper.mirror_utils.download_utils.direct_link_generator import gdtot
 from bot import dispatcher, LOGGER, STOP_DUPLICATE, download_dict, download_dict_lock, Interval
 from bot.helper.ext_utils.parser import appdrive
+from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
 from bot.helper.ext_utils.bot_utils import is_gdrive_link, is_gdtot_link, new_thread, is_appdrive_link
 
 
